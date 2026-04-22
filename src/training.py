267f81a810af515161f70a66626ffc50877900d9
kierancarroll <mr.kierancarroll@gmail.com>
2026-04-22 23:27:38 +0200

    fixed bug in training procedure

--- src/training.py
+++ src/training.py
@@ -22,7 +22,7 @@ def train(model, optimizer, criterion, epochs, x, y, train_mask, val_mask, A_hat
       correct_val = (pred[val_mask] == y[val_mask]).sum().item()
       accuracy_val = correct_val/val_mask.sum().item()
 
-    train_loss.append(loss_train)
+    train_loss.append(loss_train.item())
     val_acc.append(accuracy_val)
     print(f"Epoch: {i+1} | Train Loss: {loss_train} | Val Accuracy: {accuracy_val}")
 
