849aa0389c3968c873d2624ca6ff866e9ad04a55
kierancarroll <mr.kierancarroll@gmail.com>
2026-04-23 10:06:17 +0200

    added datasets

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,5 +1,14 @@
 from torch_geometric.datasets import Planetoid
 
-def load_data():
-    dataset = Planetoid(root='data/Planetoid', name='Cora')
+def load_data(dataset):
+    if dataset == 'Cora':
+        dataset = Planetoid(root='data/Planetoid', name='Cora')
+
+    elif dataset == 'Citeseer':
+        dataset = Planetoid(root='data/Planetoid', name='Citeseer')
+
+    else: 
+        assert dataset=="PubMed", "ERROR INVALID DATASET"
+        dataset = Planetoid(root='data/Planetoid', name='PubMed')
+
     return dataset[0]
